01b97a230fbbca6611718a23dfe585eaebe90487
kokoabdiyasno <kokoabdiyasno87@gmail.com>
2023-08-21 18:07:20 +0700

    ini dari lokal

--- resources/views/front/beranda/index.blade.php
+++ resources/views/front/beranda/index.blade.php
@@ -9,7 +9,7 @@
                     <div class="my-5 text-center text-xl-start">
                         <h1 class="display-5 fw-bolder text-white mb-2">Selamat Datang</h1>
                         <p class="lead fw-normal text-white-50 mb-4">Sistem Informasi Pengajuan dan Administrasi
-                            Surat Desa Kerta Jaya Kabupaten Musi Banyuasin, Sumatra Selatan, 30711</p>
+                            Surat Desa Kerta Jaya Kabupaten Musi Banyuasin, Sumatera Selatan, 30711</p>
                         <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
                             <a class="btn btn-primary btn-lg px-4 me-sm-1" href="#features">Pengajuan</a>
                             <a class="btn btn-outline-light btn-lg px-4" href="/hubungi-kami">Hubungi</a>
